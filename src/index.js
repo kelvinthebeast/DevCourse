2c95baa11efb32862c9fa899ecd4f95c1f2bb439
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-06-18 13:17:35 +0700

    config form data parsing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const { engine } = require('express-handlebars');
 const morgan = require('morgan')
 const path = require('path');
+const { json } = require('stream/consumers');
 
 
 app.use(express.static(path.join(__dirname, 'public'))); 
@@ -15,16 +16,25 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views'); 
 app.set('views', path.join(__dirname, 'resources/views'));
-
-console.log("Views directory set to:", path.join(__dirname, 'resources/views'));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded`
+app.use(express.json())
 app.get("/", (req, res) => {
   res.render("home", { title: "Home" });
 })
 
 app.get("/news", (req, res) => {
+  console.log(req.query)
   res.render("news", { title: "News" });
 })
 
+app.get("/search", (req, res) => {
+  console.log(req.query)
+  res.render("search", { title: "Search" });
+})
+
+app.post("/search", (req, res) => {
+  res.send("")
+})
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
