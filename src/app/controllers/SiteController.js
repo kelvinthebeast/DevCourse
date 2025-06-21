1437819eec8732a3bde72c70a628472805d19740
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-06-21 15:33:07 +0700

    Crud read

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,13 @@
+const Course = require("../../models/course.model.js")
+const { multipleMongooseToObject } = require("../../util/mongoose.js")
 class SiteController {
-    // [GET] /news
-    home(req, res) {
-        res.render('home', { title: 'Home' });
-    }
+    // [GET] /
+    home(req, res, next) {
+        Course.find({})
+            .then(courses => res.render('home', { 
+                courses: multipleMongooseToObject(courses)}))
+            .catch(next);
+        }
 
     search(req, res) {
         res.render('search', { title: 'Search' });
